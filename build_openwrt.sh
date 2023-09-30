3d72dc5acd4fe19dce8564a7dc215d779ef1fab8
PrivateRouter <101918538+PrivateRouter@users.noreply.github.com>
2023-09-29 23:07:11 -0400

    Update build_openwrt.sh

--- build_openwrt.sh
+++ build_openwrt.sh
@@ -29,10 +29,10 @@ sed -i "s/CONFIG_TARGET_ROOTFS_PARTSIZE=.*/CONFIG_TARGET_ROOTFS_PARTSIZE=$ROOTFS
 
 make image PROFILE="generic" \
         PACKAGES="kmod-rt2800-usb rt2800-usb-firmware kmod-rtl8192cu kmod-cfg80211 kmod-lib80211 kmod-mac80211 \
-           wpa-supplicant hostapd base-files block-mount fdisk luci-app-minidlna minidlna samba4-server samba4-libs \
-           luci-app-samba4 wireguard-tools luci-app-wireguard wpa-cli openvpn-openssl luci-app-openvpn watchcat \
+           wpad-mesh-openssl base-files block-mount fdisk luci-app-minidlna minidlna samba4-server samba4-libs \
+           luci-app-samba4 wireguard-tools luci-app-wireguard openvpn-openssl luci-app-openvpn watchcat \
            openssh-sftp-client luci-base luci-ssl luci-mod-admin-full luci-theme-bootstrap kmod-usb-storage kmod-usb-ohci \
-           kmod-usb-uhci e2fsprogs resize2fs htop debootstrap luci-compat luci-lib-ipkg dnsmasq luci-app-ttyd irqbalance \
+           kmod-usb-uhci e2fsprogs resize2fs htop debootstrap luci-compat luci-lib-ipkg dnsmasq-full luci-app-ttyd irqbalance \
            ethtool netperf speedtest-netperf iperf3 curl wget rsync file lsof less mc tree usbutils diffutils \
            openssh-sftp-server nano kmod-fs-exfat kmod-fs-ext4 urngd usign vpn-policy-routing wg-installer-client \
            kmod-usb-core kmod-usb3 dropbear zlib wireless-regdb f2fsck kmod-usb-wdm kmod-usb-net-ipheth usbmuxd \
