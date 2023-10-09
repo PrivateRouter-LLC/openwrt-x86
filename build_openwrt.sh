eb323b4381ab344d49ad3b5922ac72a8d24a6232
Jason Hawks <jason@torguard.net>
2023-10-09 18:11:02 -0400

    Update build

--- build_openwrt.sh
+++ build_openwrt.sh
@@ -28,9 +28,9 @@ sed -i "s/CONFIG_TARGET_KERNEL_PARTSIZE=.*/CONFIG_TARGET_KERNEL_PARTSIZE=$KERNEL
 sed -i "s/CONFIG_TARGET_ROOTFS_PARTSIZE=.*/CONFIG_TARGET_ROOTFS_PARTSIZE=$ROOTFS_PARTSIZE/g" .config
 
 make image PROFILE="generic" \
-        PACKAGES="kmod-rt2800-usb rt2800-usb-firmware kmod-rtl8192cu kmod-cfg80211 kmod-lib80211 kmod-mac80211 \
-           wpa-supplicant hostapd base-files block-mount fdisk luci-app-minidlna minidlna samba4-server samba4-libs \
-           luci-app-samba4 wireguard-tools luci-app-wireguard wpa-cli openvpn-openssl luci-app-openvpn watchcat \
+        PACKAGES="wpad kmod-rt2800-usb rt2800-usb-firmware kmod-rtl8192cu kmod-cfg80211 kmod-lib80211 kmod-mac80211 \
+           base-files block-mount fdisk luci-app-minidlna minidlna samba4-server samba4-libs \
+           luci-app-samba4 wireguard-tools luci-app-wireguard openvpn-openssl luci-app-openvpn watchcat \
            openssh-sftp-client luci-base luci-ssl luci-mod-admin-full luci-theme-bootstrap kmod-usb-storage kmod-usb-ohci \
            kmod-usb-uhci e2fsprogs resize2fs htop debootstrap luci-compat luci-lib-ipkg dnsmasq luci-app-ttyd irqbalance \
            ethtool netperf speedtest-netperf iperf3 curl wget rsync file lsof less mc tree usbutils diffutils \
@@ -38,6 +38,6 @@ make image PROFILE="generic" \
            kmod-usb-core kmod-usb3 dropbear zlib wireless-regdb f2fsck kmod-usb-wdm kmod-usb-net-ipheth usbmuxd \
            kmod-usb-net-asix-ax88179 kmod-usb-net-cdc-ether mount-utils kmod-rtl8xxxu kmod-rtl8187 rtl8188eu-firmware \
            kmod-rtl8192ce kmod-rtl8192de adblock luci-app-adblock kmod-fs-squashfs squashfs-tools-unsquashfs \
-           squashfs-tools-mksquashfs luci-app-uhttpd kmod-fs-f2fs kmod-fs-vfat git git-http jq kmod-igc bash libmpeg2 bc" \
+           squashfs-tools-mksquashfs luci-app-uhttpd kmod-fs-f2fs kmod-fs-vfat git git-http jq kmod-igc bash libmpeg2" \
         FILES="${BASEDIR}/files/" \
         BIN_DIR="${OUTPUT}"
